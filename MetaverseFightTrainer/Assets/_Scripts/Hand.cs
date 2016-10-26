0d8de6249fb985a0948e5464d3dfa3c2007d3dde
Adan Gutierrez <hevyw8@gmail.com>
2016-10-26 16:02:08 -0500

    reverted to original simple haptic pulse, left in commented out haptic pattern code.

--- MetaverseFightTrainer/Assets/_Scripts/Hand.cs
+++ MetaverseFightTrainer/Assets/_Scripts/Hand.cs
@@ -3,12 +3,21 @@ using System.Collections;
 
 [RequireComponent(typeof(SteamVR_TrackedObject))]
 public class Hand : MonoBehaviour {
-    //private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;   
     
-
-    //private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
     SteamVR_TrackedObject trackedObj;
     SteamVR_Controller.Device device;
+
+    //[Space(5)]
+    //[Header("Haptics")]
+    //[Space(5)]
+    //[Range(0, 5)]
+    //public int pulseCount;
+    //[Range(0.0f, 5.0f)]
+    //public float pulseLength;
+    //[Range(0.0f, 5.0f)]
+    //public float pauseLength;
+    
+
 	void Awake () {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
     }
@@ -20,10 +29,30 @@ public class Hand : MonoBehaviour {
     private void OnTriggerEnter(Collider col)
     {
         device.TriggerHapticPulse();
+        //StartCoroutine(HapticPatternPulse(pulseCount, pulseLength, pauseLength));
     }
 
     private void OnTriggerExit(Collider col)
     {
        
     }
+
+    //IEnumerator HapticPatternPulse(int pulseCount, float pulseLength, float pauseLength)
+    //{
+    //    for (int i = 0; i < pulseCount; i++)
+    //    {
+    //        yield return StartCoroutine(HapticSinglePulse(pulseLength));
+    //        yield return new WaitForSeconds(pauseLength);
+
+    //    }
+    //}
+
+    //IEnumerator HapticSinglePulse(float length)
+    //{
+    //    for (float i = 0; i < length; i += Time.deltaTime)
+    //    {
+    //        device.TriggerHapticPulse();
+    //        yield return null;
+    //    }
+    //}
 }
