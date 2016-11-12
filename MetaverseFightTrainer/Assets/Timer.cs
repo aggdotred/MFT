ebf06ac9b217bb112d453bf3250e1c2969458705
Adan Gutierrez <hevyw8@gmail.com>
2016-11-11 21:35:39 -0600

    implemented timer that destroys uke on countdown

---
+++ MetaverseFightTrainer/Assets/Timer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    public Text countDownText;
+    public float countDownSeconds = 60;
+    public float seconds, minutes;
+
+    private GameObject uke;
+
+	void Start () {
+        countDownText = GetComponent<Text>() as Text;
+        uke = GameObject.Find("Uke");
+	}
+	
+	
+	void Update () {
+
+        countDownSeconds -= Time.deltaTime;
+        countDownText.text = "Time Remaining: " + countDownSeconds.ToString("00");
+	    if (countDownSeconds <= 0)
+        {
+            Destroy(uke);
+            countDownSeconds = 0;
+            countDownText.text = "Time Remaining: " + countDownSeconds.ToString("00");
+        }
+	}
+}
